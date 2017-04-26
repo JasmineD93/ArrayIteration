a3578034a2609783307ecc790e3f8fa1926f0e9b
JasmineD93 <Jasmine031611@gmail.com>
2017-04-26 15:51:39 -0500

    Add files via upload

---
+++ arrayiteration.js
@@ -0,0 +1,13 @@
+//For Loop
+var colors = ["red","orange","yellow","green"];
+
+for(var i = 0; i < colors.length; i++){
+	console.log(colors[i]);
+}
+
+//For Each
+var colors = ["red","orange","yellow","green"];
+
+colors.forEach(function(){
+	console.log("Inside the foreach");
+});
